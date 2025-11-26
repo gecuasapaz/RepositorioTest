283d2529e9b15d8e6bb8fc9571452de6786bba20
gecuasapaz <gecuasapaz@pucesi.edu.ec>
2025-11-26 12:20:38 -0500

    Estructurando class

--- TestClass.java
+++ TestClass.java
@@ -3,10 +3,12 @@
 // Comentario 2 de clase
 //Comentario Nuevo
 
-public int multiplicar(int a, int b) {
-    int aux = 1;
-    int aux2 = 1;
-    aux = 3;
-    return a * b;
-}
+public class TestClass {
 
+    public int multiplicar(int a, int b) {
+        int aux = 1;
+        int aux2 = 1;
+        aux = 3;
+        return a * b;
+    }
+}
